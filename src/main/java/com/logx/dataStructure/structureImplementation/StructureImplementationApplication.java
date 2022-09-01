7d6de094d5368ab4e62997d170990203ec5903ba
Lokesh <lokesh.gadiya@atyeti.com>
2022-09-01 12:20:26 +0530

    Initial Commit

---
+++ src/main/java/com/logx/dataStructure/structureImplementation/StructureImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.logx.dataStructure.structureImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StructureImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StructureImplementationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logx/dataStructure/structureImplementation/StructureImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logx.dataStructure.structureImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StructureImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
